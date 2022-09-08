a79ff03c0337165a66c71ad65ffc0185316c9013
sergioIgnatev <sergioignatev17@gmail.com>
2022-09-08 09:54:12 +0300

    gg

---
+++ model_employeeslist.php
@@ -0,0 +1,15 @@
+{
+"database" : {
+"connection" : "localmysql",
+"sql" : "SELECT * FROM Employees",
+"orderby" : "LastName"
+},
+"filteritems" : [
+{"item" : "LastName"},
+{"item" : "FirstName"}
+],
+"sortitems" : [
+{"item" : "LastName"},
+{"item" : "FirstName"}
+]
+}
